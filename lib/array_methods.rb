ff9e3010a3d5590da7278024dd38b1e5ce2dc10c
Jesse Byers <jesse.s.byers@gmail.com>
2019-08-29 16:15:11 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,25 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+ counter=0 
+ 
+  while counter < array.length do
+    array.index(value_to_find)
+    counter += 1
+    end
+
+array.index(value_to_find)
+ 
 end
 
+
+
 def find_max_value(array)
-  # Add your solution here
+  sorted_array = array.sort
+  max_to_min = sorted_array.reverse
+max_to_min(0)
 end
+
+
+
 
 def find_min_value(array)
   # Add your solution here
